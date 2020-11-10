2d0412263004c20484b31d6aab9a54917d0cae20
MilkyMiku <admin@mooku.me>
2020-11-10 22:21:19 +0000

    Pepega Program

--- src/listeners/ReadyListener.ts
+++ src/listeners/ReadyListener.ts
@@ -50,11 +50,13 @@ async function postFood(messageID: string, client: AkairoClient) {
   } catch (error) {
     if (error.code == 10008) {
       console.error("Some twat deleted his foodpost");
+      await pendingDelete(messageID);
       return;
     }
   }
   if (message == null) {
     console.log(`Message with ID of ${messageID} not found`);
+    await pendingDelete(messageID);
     return;
   }
 
